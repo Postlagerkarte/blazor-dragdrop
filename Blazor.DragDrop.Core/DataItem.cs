a1b9d1fbf404dc5c551f006e24d2b4e253cc621c
Postlagerkarte <hannes.pavelka@gmail.com>
2019-10-03 11:55:09 +0200

    initial commit

---
+++ Blazor.DragDrop.Core/DataItem.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.DragDrop.Core
+{
+    public class DataItem
+    {
+        public RenderFragment RenderFragement { get; set; }
+
+        public int DraggableId { get; set; }
+    }
+}
