2b922879594b279cfff2d64a44c5f5103b034bbb
Postlagerkarte <hannes.pavelka@gmail.com>
2019-10-03 11:55:09 +0200

    initial commit

---
+++ Blazor.DragDrop.Core/ActiveItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.DragDrop.Core
+{
+    public class ActiveItem
+    {
+        public int SourceId { get; set; }
+        public int DraggableId { get; set; }
+    }
+}
---
+++ Blazor.DragDrop.Core/DragDropService.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.DragDrop.Core
+{
+    public class DragDropService
+    {
+        private readonly List<int> _dropzones = new List<int>();
+
+        private readonly Dictionary<int, List<DataItem>> _dic = new Dictionary<int, List<DataItem>>();
+
+        private ActiveItem _activeItem;
+
+        public event Action StateHasChanged;
+
+        private int _idCounter = 0;
+
+        public int GetId()
+        {
+            _idCounter++;
+            Debug.WriteLine($"GetId: {_idCounter}");
+            return _idCounter;
+        }
+
+        public void DropActiveItem(int targetId)
+        {
+
+            Debug.WriteLine($"DropActiveItem: target {targetId} , source: {_activeItem.SourceId} , draggable-id: {_activeItem.DraggableId}");
+            //var activeRenderFragment = _dic.Values.SelectMany(i => i).Single(x => x.DraggableId == _activeItem.DraggableId);
+            //_dic[_activeItem.SourceId].Remove(activeRenderFragment);
+            //_dic[targetId].Add(activeRenderFragment);
+            //StateHasChanged?.Invoke();
+        }
+
+        public void SetActiveItem(int sourceId, int draggableId)
+        {
+            var activeItem = new ActiveItem() { SourceId = sourceId, DraggableId = draggableId };
+            Debug.WriteLine($"SetActiveItem: SourceId: {sourceId}, DraggableId: {draggableId}");
+            _activeItem = activeItem;
+        }
+
+        public void Swap(int draggableId)
+        {
+            Debug.WriteLine($"Swap Request: draggedover id: {draggableId}");
+
+            //find bucket
+            var bucket = _dic.Where(v=>v.Value != null).Single(x => x.Value.Any(y => y.DraggableId == draggableId)).Value;
+
+            //swap
+            var dataItemForDraggedOverItem = bucket.Single(x=>x.DraggableId == draggableId);
+            var indexForDraggedOverItem = bucket.IndexOf(dataItemForDraggedOverItem);
+
+            var dataItemForActiveItem = bucket.Single(x => x.DraggableId == _activeItem.DraggableId);
+            var indexForActiveItem = bucket.IndexOf(dataItemForActiveItem);
+
+            bucket[indexForDraggedOverItem] = dataItemForActiveItem;
+            bucket[indexForActiveItem] = dataItemForDraggedOverItem;
+
+            StateHasChanged?.Invoke();
+        }
+
+        public void RegisterDropzone(int dropzoneId)
+        {
+            if (_dic.ContainsKey(dropzoneId))
+            {
+                return;
+            }
+            else
+            {
+                Debug.WriteLine($"Register: dropzone {dropzoneId}");
+                _dic.Add(dropzoneId, null);
+                _dropzones.Add(dropzoneId);
+            }
+        }
+
+        public void RegisterDraggableForDropzone(int dropzoneId, DataItem dataItem)
+        {
+            if(_dic[dropzoneId] == null)
+            {
+                _dic[dropzoneId] = new List<DataItem>();
+            }
+
+            _dic[dropzoneId].Add(dataItem);
+
+            StateHasChanged?.Invoke();
+        }
+
+         public void RegisterDraggable(int draggableId, DataItem dataItem)
+        {
+
+            if (dataItem == null)
+            {
+                if (_dic.ContainsKey(draggableId)) throw new InvalidOperationException("registering draggable twice - will  clear list");
+                Debug.WriteLine($"Register: Init {draggableId} - Null DataItem");
+                _dic.Add(draggableId, new List<DataItem>());
+            }
+            else
+            {
+                if (_dic.ContainsKey(draggableId))
+                {
+                    return;
+                }
+                else
+                {
+                    Debug.WriteLine($"Register: DataItem {dataItem.DraggableId} to belong to element {draggableId}");
+                    _dic.Add(draggableId, new List<DataItem>() { dataItem });
+                }
+            }
+        }
+
+        public bool HasDraggablesForElement(int elementId)
+        {
+            var result = _dic.ContainsKey(elementId) && _dic[elementId]?.Count > 0;
+            Debug.WriteLine($"ShouldRenderDraggable for {elementId} -> {result}");
+            return result;
+        }
+
+        public List<DataItem> GetRenderFragments(int id)
+        {
+            Debug.WriteLine($"GetRenderFragments for {id}");
+            return _dic[id];
+        }
+    }
+}
