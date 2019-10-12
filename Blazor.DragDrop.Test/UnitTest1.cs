88e063bf30d3407323f98cbab9b05a5028e37499
Postlagerkarte <hannes.pavelka@gmail.com>
2019-10-12 14:08:45 +0200

    drag inside the same dropzone

---
+++ Blazor.DragDrop.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using Blazor.DragDrop.Core;
+
+namespace Blazor.DragDrop.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var dr1 = new Draggable();
+
+            
+
+            
+
+        }
+    }
+}
