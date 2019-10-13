f377d0fa24a09a41e3abfa28081de5d7f34e0484
Postlagerkarte <hannes.pavelka@gmail.com>
2019-10-14 00:05:57 +0200

    Singleton Service Factory

---
+++ Blazor.DragDrop.Core/DragDropServiceFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Blazor.DragDrop.Core
+{
+    public class DragDropServiceFactory
+    {
+        private NavigationManager _navigationManager;
+
+        private Dictionary<string, DragDropService> _state = new Dictionary<string, DragDropService>();
+
+        public DragDropServiceFactory(NavigationManager navigationManager)
+        {
+            _navigationManager = navigationManager;
+
+            _navigationManager.LocationChanged += (s, e) =>{
+
+                _state.Remove(_state.Keys.Single(x => x != e.Location));
+            };
+        }
+        
+        public DragDropService Get() 
+        {
+            if(!_state.ContainsKey(_navigationManager.Uri))
+            {
+                _state.Add(_navigationManager.Uri, new DragDropService());
+            }
+
+            return _state[_navigationManager.Uri];
+        }
+
+    }
+}
