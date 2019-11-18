6b05fdef8ed68d989b08b1e3f6b6328874a15850
Postlagerkarte <hannes.pavelka@gmail.com>
2019-11-18 21:51:33 +0100

    add service extension

---
+++ Blazor.DragDrop.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.DragDrop.Core
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBlazorDragDrop(this IServiceCollection services)
+        {
+            return services.AddScoped<DragDropServiceFactory>();
+        }
+    }
+}
