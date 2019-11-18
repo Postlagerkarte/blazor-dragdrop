5ecf9f35a9b4512bf876df1a22d4eb6487fb790a
Postlagerkarte <hannes.pavelka@gmail.com>
2019-11-18 21:51:33 +0100

    add service extension

--- Blazor.DragDrop.Demo/Startup.cs
+++ Blazor.DragDrop.Demo/Startup.cs
@@ -31,7 +31,8 @@ namespace Blazor.DragDrop.Demo
             {
                 o.DetailedErrors = true;
             });
-            services.AddScoped<DragDropServiceFactory>();
+            
+            services.AddBlazorDragDrop();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
