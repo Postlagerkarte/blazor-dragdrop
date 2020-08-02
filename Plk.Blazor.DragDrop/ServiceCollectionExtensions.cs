d2b77511461bd0097bd0455d64ff95d11b0a87ab
postlagerkarte <hannes.pavelka@gmail.com>
2020-08-02 23:04:23 +0200

    v2 initial

---
+++ Plk.Blazor.DragDrop/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plk.Blazor.DragDrop
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBlazorDragDrop(this IServiceCollection services)
+        {
+            return services.AddScoped(typeof(DragDropService<>));
+        }
+    }
+}
