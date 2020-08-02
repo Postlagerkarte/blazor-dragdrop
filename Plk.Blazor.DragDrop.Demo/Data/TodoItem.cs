86c07ff1c77c44713f891f2773e7b2ab4be0b43a
postlagerkarte <hannes.pavelka@gmail.com>
2020-08-02 23:04:23 +0200

    v2 initial

---
+++ Plk.Blazor.DragDrop.Demo/Data/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plk.Blazor.DragDrop.Demo.Data
+{
+    public class TodoItem
+    {
+        public string Titel { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ Plk.Blazor.DragDrop/DragDropService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Plk.Blazor.DragDrop
+{
+    internal class DragDropService<T>
+    {
+        public T ActiveItem { get; set; }
+        public List<T> Items { get; set; }
+    }
+}
