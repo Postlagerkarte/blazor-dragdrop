0d629906f0423c9d9b27202a74bb693cfefcdcca
postlagerkarte <hannes.pavelka@gmail.com>
2020-08-03 22:01:41 +0200

    add styles

---
+++ Plk.Blazor.DragDrop/DragDropStyle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plk.Blazor.DragDrop
+{
+    public enum DragDropStyle
+    {
+        Custom,
+        Default
+    }
+}
