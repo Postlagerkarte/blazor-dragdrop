b7eefb6066a38d59219f0378a78057d7f2836d13
j1shin <cradle-of-mail@gmx.de>
2021-03-09 15:13:05 +0100

    Postlagerkarte/blazor-dragdrop#106 Dropzone, removed OnSelectDropAction parameter in favor of child component approach

--- Plk.Blazor.DragDrop/DropActions/DropActionContext.cs
+++ Plk.Blazor.DragDrop/DropActions/DropActionContext.cs
@@ -1,16 +1,13 @@
 using System.Collections.Generic;
-using Plk.Blazor.DragDrop.DropActions;
 
-namespace Plk.Blazor.DragDrop
+namespace Plk.Blazor.DragDrop.DropActions
 {
-    public class DropzoneSelectDropActionArgs<TItem>
+    public class DropActionContext<TItem>
     {
         public Dropzone<TItem> Sender { get; set; }
         public TItem ActiveItem { get; set; }
         public IList<TItem> SourceItems { get; set; }
         public IList<TItem> TargetItems { get; set; }
         public int? TargetItemIndex { get; set; }
-
-        public IDropAction<TItem> DropAction { get; set; }
     }
 }
--- Plk.Blazor.DragDrop/DropActions/IDropAction.cs
+++ Plk.Blazor.DragDrop/DropActions/IDropAction.cs
@@ -1,10 +1,12 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 
 namespace Plk.Blazor.DragDrop.DropActions
 {
     public interface IDropAction<TItem>
     {
-        void Execute(TItem activeItem, IList<TItem> sourceItems, IList<TItem> targetItems, int? targetItemIndex);
+        Func<DropActionContext<TItem>, bool> IsSuitable { get; set; }
+
+        void Execute(DropActionContext<TItem> context);
     }
 }
