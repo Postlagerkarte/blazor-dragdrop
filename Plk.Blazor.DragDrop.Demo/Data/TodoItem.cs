9b3afba19dac9c0b03c569408d24559aba4f36da
GioviQ <giovanni@quarella.net>
2021-10-22 21:53:00 +0200

    typo

--- Plk.Blazor.DragDrop.Demo/Data/TodoItem.cs
+++ Plk.Blazor.DragDrop.Demo/Data/TodoItem.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Plk.Blazor.DragDrop.Demo.Data
+﻿namespace Plk.Blazor.DragDrop.Demo.Data
 {
     public class TodoItem
     {
-        public string Titel { get; set; }
+        public string Title { get; set; }
         public bool IsDone { get; set; }
         public bool IsNew { get; set; }
     }
